496a26a7b1ac07d03e241c9db48b84b72845b356
Harold Gomez <xagobo14@gmail.com>
2021-10-08 12:50:03 -0500

    midificar variables HG

--- FivewareBack/src/main/java/co/edu/unbosque/model/Ventas.java
+++ FivewareBack/src/main/java/co/edu/unbosque/model/Ventas.java
@@ -7,18 +7,18 @@ import javax.persistence.Id;
 public class Ventas {
 	
 	@Id
-	private Long codigo_ventas;
+	private Long codigo_venta;
 	private Long cedula_cliente;
 	private Long cedula_usuario;
-	private double ivaventa;
-	private double total_venta;
-	private double valor_venta;
+	private Double ivaventa;
+	private Double total_venta;
+	private Double valor_venta;
 	
-	public Long getCodigo_ventas() {
-		return codigo_ventas;
+	public Long getCodigo_venta() {
+		return codigo_venta;
 	}
-	public void setCodigo_ventas(Long codigo_ventas) {
-		this.codigo_ventas = codigo_ventas;
+	public void setCodigo_venta(Long codigo_venta) {
+		this.codigo_venta = codigo_venta;
 	}
 	public Long getCedula_cliente() {
 		return cedula_cliente;
@@ -32,24 +32,22 @@ public class Ventas {
 	public void setCedula_usuario(Long cedula_usuario) {
 		this.cedula_usuario = cedula_usuario;
 	}
-	public double getIvaventa() {
+	public Double getIvaventa() {
 		return ivaventa;
 	}
-	public void setIvaventa(double ivaventa) {
+	public void setIvaventa(Double ivaventa) {
 		this.ivaventa = ivaventa;
 	}
-	public double getTotal_venta() {
+	public Double getTotal_venta() {
 		return total_venta;
 	}
-	public void setTotal_venta(double total_venta) {
+	public void setTotal_venta(Double total_venta) {
 		this.total_venta = total_venta;
 	}
-	public double getValor_venta() {
+	public Double getValor_venta() {
 		return valor_venta;
 	}
-	public void setValor_venta(double valor_venta) {
+	public void setValor_venta(Double valor_venta) {
 		this.valor_venta = valor_venta;
 	}
-	
-
 }
